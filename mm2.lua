e1e57556f01a365c7434fa0ae274f1ebebf83c7a
devpickletalk <devpickletalk@gmail.com>
2026-04-22 22:39:38 +0800

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -888,7 +888,7 @@ do
 
     local frame = Instance.new("Frame")
     frame.Size                   = UDim2.new(0, 170, 0, 60)
-    frame.Position               = UDim2.new(1, -180, 0, 80)
+    frame.Position               = UDim2.new(1, -180, 0, 10)
     frame.BackgroundTransparency = 1
     frame.Parent                 = gui
 
